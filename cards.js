4320f0d474d817f231d6427208e146da721b4a49
kcgidw <kc.gideonw@gmail.com>
2017-08-22 23:21:31 -0700

    initial commit

---
+++ cards.js
@@ -0,0 +1,82 @@
+var fs = require("fs");
+var _ = require("lodash");
+
+var fields = [
+	"name",
+	"factionId",
+	"type",
+	"rarity",
+	"race",
+	"cost",
+	"attack",
+	"health",
+	"description"
+];
+var factions = ["LYON", "SONG", "VETR", "ABYS", "MAGM", "VANA"];
+
+var encodeCard = (card) => {
+	var str = "";
+	_.each(fields, prop => {
+	str += card[prop] + "|";
+	});
+	return str;
+};
+
+var decodeVal = val => {
+	if(val === 'null') {
+		return null;
+	}
+	if(!isNaN(Number.parseInt(val))) {
+		return Number.parseInt(val);
+	}
+	return val;
+}
+var decodeLine = str => {
+	var vals = str.split("|", fields.length);
+	var card = {};
+
+	card.name = decodeVal(vals[0]);
+	var factionId = decodeVal(vals[1]);
+	card.faction = factions[factionId -1];
+	card.type = decodeVal(vals[2]);
+	card.rarity = decodeVal(vals[3]);
+	card.race = decodeVal(vals[4]);
+	card.cost = decodeVal(vals[5]);
+	card.attack = decodeVal(vals[6]);
+	card.health = decodeVal(vals[7]);
+	card.description = decodeVal(vals[8]);
+
+	return card;
+};
+
+var infile = process.argv[2];
+var outfile = process.argv[3];
+console.log(infile + " -> " + outfile);
+
+var input, output;
+
+if (infile.match(/.*\.json$/)) {
+	input = require('./' + infile);
+	var cardData = input.cardData;
+	lines = _.map(cardData, card => encodeCard(card));
+
+	output = '';
+	_.each(lines, line => {
+		output += line + '\r\n';
+	});
+} else {
+	input = fs.readFileSync('./' + infile, 'utf8');
+	var lines = input.split("\n");
+
+	output = [];
+	_.each(lines, line => {
+		output.push(decodeLine(line));
+	});
+	output = JSON.stringify(output);
+}
+
+if (outfile) {
+	fs.writeFile(outfile, output);
+} else {
+	console.log(output);
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+cards.js [infile path] [outfile path]
+
+If infile is JSON, will encode into txt. Else, will decode into JSON.
+
+If outfile unspecified, will print to stdout.
+
+See datafiles/ folder for example input and output files.
+
+Data taken from https://duelyststats.info/scripts/carddata/cardData.json
