6d1bde29c048ec1c432f822e56c44b35077b7dd8
kcgidw <kc.gideonw@gmail.com>
2017-08-23 18:42:10 -0700

    flags

--- cards.js
+++ cards.js
@@ -1,5 +1,8 @@
 var fs = require("fs");
 var _ = require("lodash");
+var argv = require('optimist').argv;
+
+/* */
 
 var fields = [
 	"name",
@@ -17,7 +20,7 @@ var factions = ["LYON", "SONG", "VETR", "ABYS", "MAGM", "VANA"];
 var encodeCard = (card) => {
 	var str = "";
 	_.each(fields, prop => {
-	str += card[prop] + "|";
+		str += card[prop] + "|";
 	});
 	return str;
 };
@@ -30,9 +33,14 @@ var decodeVal = val => {
 		return Number.parseInt(val);
 	}
 	return val;
-}
+};
 var decodeLine = str => {
 	var vals = str.split("|", fields.length);
+
+	if(vals.length < fields.length) {
+		return undefined;
+	}
+
 	var card = {};
 
 	card.name = decodeVal(vals[0]);
@@ -49,30 +57,58 @@ var decodeLine = str => {
 	return card;
 };
 
-var infile = process.argv[2];
-var outfile = process.argv[3];
-console.log(infile + " -> " + outfile);
+/* */
 
-var input, output;
+var infile = argv._[0];
+var outfile = argv._[1];
+var shuffle = argv.shuffle;
+var copies = argv.copies || 1;
+//console.log(argv);
+
+var input, output, action;
 
 if (infile.match(/.*\.json$/)) {
+	action = 'ENCODE';
+
 	input = require('./' + infile);
 	var cardData = input.cardData;
 	lines = _.map(cardData, card => encodeCard(card));
+
+	if(copies > 1) {
+		var lines2 = [];
+		for(var i=0; i<copies; i++) {
+			lines2 = _.concat(lines, lines2);
+		}
+		lines = lines2;
+	}
+	if(shuffle) {
+		lines = _.shuffle(lines);
+	}
 
 	output = '';
 	_.each(lines, line => {
 		output += line + '\r\n';
 	});
+	
 } else {
+	action = 'DECODE';
+
 	input = fs.readFileSync('./' + infile, 'utf8');
-	var lines = input.split("\n");
+	var lines = input.split("\r\n");
+
+	if(shuffle) {
+		lines = _.shuffle(lines);
+	}
 
 	output = [];
 	_.each(lines, line => {
-		output.push(decodeLine(line));
+		var decodedLine = decodeLine(line);
+		if(decodedLine) {
+			output.push(decodedLine);
+		}
 	});
-	output = JSON.stringify({cards: output});
+
+	output = JSON.stringify({cards: output}, null, 2);
 }
 
 if (outfile) {
@@ -80,3 +116,5 @@ if (outfile) {
 } else {
 	console.log(output);
 }
+
+console.log(action + ' ' + infile + " -> " + outfile);
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-`node cards [infile path] [outfile path]`
+`node cards infile_path outfile_path --shuffle --copies X`
 
 If infile is JSON, will encode into txt. Else, will decode into JSON.
 
 If outfile unspecified, will print to stdout.
 
 See datafiles/ folder for example input and output files.
+
+Shuffle: Shuffles the resulting data
+
+Copies: Repeats the data X times. Only applies to encoding
 
 Data taken from https://duelyststats.info/scripts/carddata/cardData.json
